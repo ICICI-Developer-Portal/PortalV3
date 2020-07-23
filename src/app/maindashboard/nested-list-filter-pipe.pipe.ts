083ab34fa67a45793bb36eaacc802a3747eb5c58
Shikha Pandey (INDIA PRE BFSI) <SH20118239@wipro.com>
2020-07-23 20:25:34 +0530

    filter.ts

--- src/app/maindashboard/nested-list-filter-pipe.pipe.ts
+++ src/app/maindashboard/nested-list-filter-pipe.pipe.ts
@@ -4,9 +4,21 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'nestedListFilterPipe'
 })
 export class NestedListFilterPipePipe implements PipeTransform {
+ 
+  transform(responseData: any, term: any): any {
+console.log(responseData,"reached");
+    //check if search term is undefined
+    if(term === undefined) {
+     
+      return responseData;
+    } 
+    //return updates responseData array
+    return responseData.filter(function(thisresponseData){
+      console.log(thisresponseData)
+      console.log(thisresponseData.name.toLowerCase().includes(term.toLowerCase()))
+      return thisresponseData.name.toLowerCase().includes(term.toLowerCase());
+    }) 
 
-  transform(value: any, args?: any): any {
-    return null;
   }
 
 }
