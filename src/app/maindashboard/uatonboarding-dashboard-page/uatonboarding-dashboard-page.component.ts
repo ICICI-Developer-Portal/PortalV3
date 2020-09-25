1338d47e5bbc7f0a7ad891032e80be7eac1c9ffa
Shikha Pandey (INDIA PRE BFSI) <SH20118239@wipro.com>
2020-09-25 19:16:20 +0530

    prod build

--- src/app/maindashboard/uatonboarding-dashboard-page/uatonboarding-dashboard-page.component.ts
+++ src/app/maindashboard/uatonboarding-dashboard-page/uatonboarding-dashboard-page.component.ts
@@ -647,52 +647,84 @@ else{
     //console.log(reactiveFromFieldValues.value.Ip);
 
     const formData = new FormData();
-    formData.append("userName", inputFields["userName"]);
-    formData.append("domainName", inputFields["domainName"]);
-    formData.append("domainApis", inputFields["domainApis"]);
-    formData.append("mName", inputFields["mName"]);
-    formData.append("desc", inputFields["desc"]);
-    formData.append("spocEmail", inputFields["spocEmail"]);
-    formData.append("spocPhone", inputFields["spocPhone"]);
-    formData.append("relManager", inputFields["relManager"]);
-    formData.append("env", inputFields["env"]);
-    formData.append("refundCode", inputFields["refundCode"]);
-    formData.append("callbackUrl", inputFields["callbackUrl"]);
-    formData.append("AccountNo", inputFields["AccountNo"]);
-    formData.append("ClientCode", inputFields["ClientCode"]);
-    formData.append("url", inputFields["url"]);
-    formData.append("Ip", inputFields["Ip"]);
-    formData.append("Port", inputFields["Port"]);
-    formData.append("Checksum", inputFields["Checksum"]);
-    formData.append("Encryption", inputFields["Encryption"]);
-    formData.append("Certificate", inputFields["Certificate"]);
-    formData.append("web", inputFields["web"]);
-    formData.append("message", inputFields["message"]);
-    formData.append("IFSC_Code", inputFields["IFSC_Code"]);
-    formData.append("virtualCode", inputFields["virtualCode"]);
-    formData.append("refundCode", inputFields["refundCode"]);
-    formData.append("Account_no", inputFields["Account_no"]);
-    formData.append("Acc_name", inputFields["Acc_name"]);
-    formData.append("Auth_level", inputFields["Auth_level"]);
-    formData.append("Urn", inputFields["Urn"]);
-    formData.append("Acc_env", inputFields["Acc_env"]);
-    formData.append("Acc_validation", inputFields["Acc_validation"]);
-    formData.append("Acc_acceptance", inputFields["Acc_acceptance"]);
-    formData.append("Rec_mail", inputFields["Rec_mail"]);
-    formData.append("Acc_mode", inputFields["Acc_mode"]);
-    formData.append("Acc_trans", inputFields["Acc_trans"]);
-    formData.append("Acc_amount", inputFields["Acc_amount"]);
-    formData.append("Acc_headers", inputFields["Acc_headers"]);
-    formData.append("Acc_uatTestingID", inputFields["Acc_uatTestingID"]);
+    formData.append("userName", inputFields["userName"]); //1
+    formData.append("domainName", inputFields["domainName"]); //2
+    formData.append("domainApis", inputFields["domainApis"]); //3
+    formData.append("mName", inputFields["mName"]);  //4
+    formData.append("desc", inputFields["desc"]); //5
+    formData.append("spocEmail", inputFields["spocEmail"]); //6
+    formData.append("spocPhone", inputFields["spocPhone"]); //7
+    formData.append("relManager", inputFields["relManager"]); //8
+    formData.append("env", inputFields["env"]); //9
+    formData.append("AccountNo", inputFields["AccountNo"]); //10
+    formData.append("ClientCode", inputFields["ClientCode"]); //11
+    formData.append("url", inputFields["url"]); //12
+    formData.append("Ip", inputFields["Ip"]);  //13
+    formData.append("Port", inputFields["Port"]);  //14
+    formData.append("Checksum", inputFields["Checksum"]);  //15
+    formData.append("Encryption", inputFields["Encryption"]); //16
+    formData.append("Certificate", inputFields["Certificate"]); //17
+    formData.append("web", inputFields["web"]); //18
+    formData.append("message", inputFields["message"]); //19
+    formData.append("IFSC_Code", inputFields["IFSC_Code"]); //20
+    formData.append("virtualCode", inputFields["virtualCode"]); //21
+    formData.append("refundCode", inputFields["refundCode"]); //22
+    formData.append("Account_no", inputFields["Account_no"]); //23
+    formData.append("Acc_name", inputFields["Acc_name"]);//24
+    formData.append("Auth_level", inputFields["Auth_level"]);//25
+    formData.append("Urn", inputFields["Urn"]);//26
+    formData.append("Acc_env", inputFields["Acc_env"]); //27
+    formData.append("Acc_validation", inputFields["Acc_validation"]); //28
+    formData.append("Acc_acceptance", inputFields["Acc_acceptance"]);//29
+    formData.append("Rec_mail", inputFields["Rec_mail"]);//30
+    formData.append("Acc_mode", inputFields["Acc_mode"]);//31
+    formData.append("Acc_trans", inputFields["Acc_trans"]);//32
+    formData.append("Acc_amount", inputFields["Acc_amount"]);//33
+    // formData.append("Acc_headers", inputFields["Acc_headers"]);
+    // formData.append("Acc_uatTestingID", inputFields["Acc_uatTestingID"]);
+
+
+
+
+
+    // formData.append("refundCode", inputFields["refundCode"]);
+    // formData.append("callbackUrl", inputFields["callbackUrl"]);
+    // formData.append("AccountNo", inputFields["AccountNo"]);
+    // formData.append("ClientCode", inputFields["ClientCode"]);
+    // formData.append("url", inputFields["url"]);
+    // formData.append("Ip", inputFields["Ip"]);
+    // formData.append("Port", inputFields["Port"]);
+    // formData.append("Checksum", inputFields["Checksum"]);
+    // formData.append("Encryption", inputFields["Encryption"]);
+    // formData.append("Certificate", inputFields["Certificate"]);
+    // formData.append("web", inputFields["web"]);
+    // formData.append("message", inputFields["message"]);
+    // formData.append("IFSC_Code", inputFields["IFSC_Code"]);
+    // formData.append("virtualCode", inputFields["virtualCode"]);
+    // formData.append("refundCode", inputFields["refundCode"]);
+    // formData.append("Account_no", inputFields["Account_no"]);
+    // formData.append("Acc_name", inputFields["Acc_name"]);
+    // formData.append("Auth_level", inputFields["Auth_level"]);
+    // formData.append("Urn", inputFields["Urn"]);
+    // formData.append("Acc_env", inputFields["Acc_env"]);
+    // formData.append("Acc_validation", inputFields["Acc_validation"]);
+    // formData.append("Acc_acceptance", inputFields["Acc_acceptance"]);
+    // formData.append("Rec_mail", inputFields["Rec_mail"]);
+    // formData.append("Acc_mode", inputFields["Acc_mode"]);
+    // formData.append("Acc_trans", inputFields["Acc_trans"]);
+    
     console.log(formData);
     let a: any = (<HTMLInputElement>document.getElementById("file1")).files;
     console.log("a", a);
     for (let k = 0; k < a.length; k++) {
-      formData.append("file1", a[k]);
+      formData.append("file1", a[k]); //34
       console.log(a[k], "oooooooooo")
       console.log(formData)
     }
     // Appended three new elements
+    // formData.append("Acc_amount", inputFields["Acc_amount"]);
+    // formData.append("Acc_headers", inputFields["Acc_headers"]);
+    // formData.append("Acc_uatTestingID", inputFields["Acc_uatTestingID"]);
 
     formData.append("refJIRAID", inputFields["Acc_refJIRAID"]);
     formData.append("Headers", inputFields["Acc_headers"]);
