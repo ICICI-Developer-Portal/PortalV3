b97fb86d12a660c55ccc9a77439a6e4de7c32b5c
Shikha Pandey <shikha.pandey3@wipro.com>
2021-10-09 01:05:24 +0530

    push prod

--- bkupsrc/app/documentation-dashb/Sidenav-pages/view-all-api-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ViewAllApiFilterPipe } from './view-all-api-filter.pipe';
-
-describe('ViewAllApiFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ViewAllApiFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
